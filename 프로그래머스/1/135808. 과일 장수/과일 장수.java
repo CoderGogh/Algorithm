a6a5563cc48e7a3974c970d532262b57b044634e
Haram Jeong <haram1021@gmail.com>
2026-04-09 21:02:08 +0900

    [level 1] Title: 과일 장수, Time: 92.10 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,14 +1,7 @@
 import java.util.*; 
 class Solution {
         public int solution(int k, int m, int[] score) {
-            // k : 최고 점수, m : 묶어 팔 수 있는 갯수, score : 사과 점수 배열
             int answer = 0;
-            // 1, 1, 2, 2, 3
-            // m = 3 -> 앞 두 개 버림
-            // m = 2 -> 앞 한 개 버림
-            // 1, 2, 2, 2, 3
-            // m = 3 -> 앞 두 개 버림
-            // m = 2 -> 앞 한 개 버림
             Arrays.sort(score);
             int r = score.length%m;
             if(score.length%m != 0){//나머지가 있는 경우 --> 앞에서부터 버림
