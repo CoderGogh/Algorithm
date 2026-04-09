15aea265a5cb79324eb126943a8f4ca9ac49fe5c
Haram Jeong <haram1021@gmail.com>
2026-04-09 18:21:36 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 12.33 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,31 +1,14 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import java.util.*;
 class Solution {
         public int[] solution(String s) {
-            int indx = s.length();
-            int[] answer = new int[indx];
-            List<String> list = new ArrayList<>();
+            int[] answer = new int[s.length()];
 
-            Arrays.stream(s.split("")).forEach(item ->list.add(item));
-
-            for (int i = 0; i < indx; i++) {
-                String now = s.substring(i,i+1);
-                if(i == 0){
-                    answer[i] = -1;
-                }else{
-                    int found = 0;
-                    for (int j = i-1; j >=0 ; j--) {
-                        String val = list.get(j);
-                        if(val.equals(now)){
-                            answer[i] = i-j;
-                            found=1;
-                            break;
-                        }
-                    }
-                    if(found==0){
-                        answer[i] = -1;
+            Arrays.fill(answer,-1);
+            for (int i = 0; i < answer.length; i++) {
+                for (int j = i-1; j >=0 ; j--) {
+                    if(s.charAt(j) == s.charAt(i)){
+                        answer[i]= i-j;
+                        break;
                     }
                 }
             }
