20bbf52de9a3a3e8611376cfb8ddb4ff156b0a6a
Haram Jeong <haram1021@gmail.com>
2026-04-11 18:57:37 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 289.04 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,28 +2,14 @@ import java.util.*;
 class Solution {
         public String solution(String[] participant, String[] completion) {
             String answer = "";
-
-            HashMap<String, Integer> map = new HashMap<>();
-            for (String s : participant
-            ) {
-                if (map.containsKey(s)) {
-                    int a = map.get(s);
-                    map.put(s, a + 1);
-                    continue;
-                }
-                map.put(s, 1);
-            }
-            for (String s: completion
-                 ) {
-                map.put(s, map.get(s) -1);
-            }
-            for (String s: map.keySet()
-                 ) {
-                if(map.get(s)!=0){
-                    answer= s;
-                    break;
+            Arrays.sort(participant);
+            Arrays.sort(completion);
+            for (int i = 0; i < completion.length; i++) {
+                if(!participant[i].equals(completion[i])) {
+                    return participant[i];
                 }
             }
+            answer = participant[participant.length-1];
             return answer;
         }
     }
