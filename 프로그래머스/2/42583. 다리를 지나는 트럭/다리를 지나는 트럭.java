8480fd7d88b52b72aa5bc3305e06949a16cf956e
Haram Jeong <haram1021@gmail.com>
2026-04-14 19:32:03 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 21.76 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+        public int solution(int bridge_length, int weight, int[] truck_weights) {
+            int time = 0;
+            int currentWeight = 0;
+            int truckIdx = 0;
+            Queue<Integer> q = new ArrayDeque<>();
+            for (int i = 0; i < bridge_length; i++) {
+                q.add(0);
+            }
+            while(truckIdx< truck_weights.length){
+                time++;
+                currentWeight -= q.poll();
+                if(currentWeight + truck_weights[truckIdx]<= weight){
+                    q.offer(truck_weights[truckIdx]);
+                    currentWeight += truck_weights[truckIdx];
+                    truckIdx++;
+                }else{
+                    q.offer(0);
+                }
+            }
+
+            return time + bridge_length;
+        }
+    }
