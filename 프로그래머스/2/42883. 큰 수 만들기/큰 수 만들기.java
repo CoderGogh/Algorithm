671ea89a4f15c0e5a3ad21ce9fa5eff8885f7cd6
Haram Jeong <haram1021@gmail.com>
2026-04-14 23:50:19 +0900

    [level 2] Title: 큰 수 만들기, Time: 65.62 ms, Memory: 99.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+        public String solution(String number, int k) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < number.length(); i++) {
+                char c = number.charAt(i);
+                while(!stack.isEmpty() && stack.peek() < c && k>0){
+                    stack.pop();
+                    k--;
+                }
+                stack.push(c);
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < stack.size() - k; i++) {
+                sb.append(stack.get(i));
+            }
+            return sb.toString();
+        }
+    }
