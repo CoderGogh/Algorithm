b96d159972ebf72be37f7a32add15f7409ff88e6
Haram Jeong <haram1021@gmail.com>
2026-04-16 18:09:13 +0900

    [level 2] Title: 괄호 회전하기, Time: 21.79 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,39 +1,73 @@
-import java.util.*;
 class Solution {
         public int solution(String s) {
             int answer = 0;
-            String rotated = s;
+            Stack stack = new Stack(s.length());
             for (int i = 0; i < s.length(); i++) {
-                if(isValid(rotated)){
+                String rotate  = s.substring(i) + s.substring(0,i);
+                if(isValid(rotate,stack)){
                     answer++;
                 }
-                rotated = rotated.substring(1)+ rotated.charAt(0);
+                stack.clear();
             }
+
             return answer;
         }
-        private boolean isValid(String s){
-            Stack<Character> stack = new Stack<>();
-
-            for (char c :
-                    s.toCharArray()) {
-                if(c == '(' || c == '{' || c == '['){
+        private boolean isValid(String s, Stack stack){
+            for (char c: s.toCharArray()) {
+                if(c == '(' || c =='{' || c == '['){
                     stack.push(c);
                 }else{
                     if(stack.isEmpty()){
                         return false;
                     }
-                    char top = stack.pop();
+                    char top = stack.peek();
                     if(c == ')' && top != '('){
                         return false;
                     }
-                    if(c == '}' && top != '{') {
+                    if(c == '}' && top != '{'){
                         return false;
                     }
                     if(c == ']' && top != '['){
                         return false;
                     }
+                    stack.pop();
                 }
             }
             return stack.isEmpty();
+        }
+        public static class Stack{
+            int top = -1;
+            char[] stack;
+
+            public Stack(int value) {
+                stack = new char[value];
+            }
+            private char pop(){
+                if(isEmpty()){
+                   throw new RuntimeException("Stack is empty");
+                }
+                return stack[top--];
+            }
+            private void push(char x){
+                if(isFull()){
+                    throw new RuntimeException("Stack Overflow");
+                }
+                stack[++top] = x;
+            }
+            private char peek(){
+                if(isEmpty()){
+                    throw new RuntimeException("Stack is empty");
+                }
+                return stack[top];
+            }
+            private boolean isFull(){
+                return top == stack.length - 1;
+            }
+            private boolean isEmpty(){
+                return top == -1;
+            }
+            private void clear(){
+                top = -1;
+            }
         }
     }
