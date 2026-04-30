971e9b25ca8ee0dd894497af29e458ec3b592dab
Haram Jeong <haram1021@gmail.com>
2026-04-30 18:11:26 +0900

    [level 3] Title: 네트워크, Time: 0.46 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,43 +1,29 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
-class Solution {
-        boolean[] visited;   // dfs
-        Deque<Integer> queue;
-        public int solution(int n, int[][] computers) {
+class Solution{
+        boolean[] visited;
+        public int solution(int n, int[][]computers) {
             int answer = 0;
             visited = new boolean[n];
-
             for (int i = 0; i < n; i++) {
                 if(!visited[i]){
                     answer++;
+                    bfs(i,n,computers);
                 }
-                // DFS
-                dfs(i,n, computers);
-                // BFS
-//                bfs(i,n, computers);
             }
-
             return answer;
         }
-        public void dfs(int node, int n, int[][] computers){
-            visited[node] = true;
-            for (int i = 0; i < n; i++) {
-                if(computers[node][i] == 1 && !visited[i]){
-                    dfs(i,n,computers);
-                }
-            }
-        }
-        public void bfs(int startNode, int n, int[][] computers){
-            Deque<Integer>queue = new ArrayDeque<>();
-            queue.offer(startNode);
+        public void bfs(int startNode,int n, int[][]computers){
+            Deque<Integer> queue = new ArrayDeque<>();
             visited[startNode] = true;
+            queue.offer(startNode);
 
             while(!queue.isEmpty()){
-                int curr =  queue.poll();
-                for (int j = 0; j < n; j++) {
-                    if(computers[curr][j] == 1 & !visited[j]){
-                        visited[j] = true;
-                        queue.offer(j);
+                int node = queue.poll();
+                for (int i = 0; i < n; i++) {
+                    if(computers[node][i] == 1 && !visited[i]){
+                        queue.offer(i);
+                        visited[i] = true;
                     }
                 }
             }
