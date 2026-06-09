db7c7466c67b28692c25a0e9f5add9cfdbce788d
Haram Jeong <haram1021@gmail.com>
2026-06-09 18:01:58 +0900

    [level 2] Title: 올바른 괄호, Time: 5.54 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,41 @@
-import java.util.*;
 class Solution {
-        Deque<Character> stack = new ArrayDeque<>();
-        boolean solution(String s) {
-            boolean answer = true;
-            for (int i = 0; i < s.length(); i++) {
-                char c = s.charAt(i);
-                if(c == '('){
-                    stack.push(c);
-                }else if(c == ')'){
-                    if(stack.isEmpty()){
-                        return false;
-                    }
-                    if(stack.peek() == '('){
-                        stack.pop();
-                    }
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack stack = new Stack(s.length());
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '('){
+                stack.push(c);
+            }else{
+                if(!stack.isEmpty() && stack.peek() == '('){
+                    stack.pop();
+                }else{
+                    answer = false;
+                    break;
                 }
             }
-            return stack.isEmpty() ? answer : false;
         }
+        return stack.isEmpty() ? answer : false;
     }
+    class Stack{
+        int top = -1;
+        char[] stack;
+        public Stack(int n){
+            stack = new char[n+1];
+        }
+        void push(char x){
+            stack[++top] = x;
+        }
+        char pop(){
+            return stack[top--];
+        }
+        boolean isEmpty(){
+            return top == -1;
+        }
+        char peek(){
+            return stack[top];
+        }
+
+    }
+
+}
