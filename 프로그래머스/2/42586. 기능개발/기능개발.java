734bc03ac1a6e866526abdeebe945af835d3530b
Haram Jeong <haram1021@gmail.com>
2026-09-04 12:57:00 +0900

    [level 2] Title: 기능개발, Time: 2.99 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,25 +1,28 @@
 import java.util.*;
 class Solution {
-        public int[] solution(int[] progresses, int[] speeds) {
-            List<Integer> answer = new ArrayList<>();
-            Queue<Integer> queue = new ArrayDeque<>();
-            for (int i = 0; i < progresses.length; i++) {
-                for (int j = 1; j <= 100; j++) {
-                    if(progresses[i] + speeds[i] * j >=100){
-                        queue.add(j);
-                        break;
-                    }
-                }
-            }
-            while(!queue.isEmpty()){
-                int beforeMax = queue.poll();
-                int count = 1;
-                while(!queue.isEmpty() && queue.peek() <= beforeMax){
-                    queue.poll();
-                    count++;
-                }
-                answer.add(count);
-            }
-            return answer.stream().mapToInt(Integer::intValue).toArray();
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] day = new int[progresses.length];
+        
+        for(int i = 0; i< progresses.length; i++){
+            int remain = 100 - progresses[i];
+            day[i] = (remain + speeds[i] -1 ) / speeds[i];
         }
+        List<Integer> answer = new ArrayList<>();
+
+        int current = day[0];
+        int count = 1;
+        
+        for(int i=1;i<day.length; i++){
+            if(day[i]<= current){
+                count ++;
+            }
+            else{
+                answer.add(count);
+                count = 1;
+                current = day[i];
+            }
+        }
+        answer.add(count);
+        return answer.stream().mapToInt(i->i).toArray();
     }
+}
