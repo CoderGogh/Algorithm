1af6d04febf6ea409d733d4d80c184e52e48239e
Haram Jeong <haram1021@gmail.com>
2026-09-04 11:52:40 +0900

    [level 3] Title: 베스트앨범, Time: 4.08 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,49 +1,44 @@
 import java.util.*;
+
 class Solution {
-        HashMap<String, Integer> map = new HashMap<>();  // 장르별 총 재생횟수
-        HashMap<String, List<Music>> genreMap = new HashMap<>();  // 장르
-
-        public int[] solution(String[] genres, int[] plays) {
-
-            for(int i=0;i<genres.length;i++){
-                String g = genres[i];
-                int p = plays[i];
-
-                // 장르별 총 합산용
-                map.put(g, map.getOrDefault(g, 0) + p);
-
-                // 장르별 곡 리스트
-                genreMap.putIfAbsent(g,new ArrayList<>());
-                genreMap.get(g).add(new Music(i,p));
-            }
-            List<String> sorted = new ArrayList<>(map.keySet());
-            sorted.sort((a,b) -> map.get(b) - map.get(a));  // 내림차순
-
-            List<Integer> result = new ArrayList<>();
-            for (String g : sorted) {
-                List<Music> musics = genreMap.get(g);
-                Collections.sort(musics);
-
-                result.add(musics.get(0).id);
-                if(musics.size() > 1){
-                    result.add(musics.get(1).id);
-                }
-            }
-            return result.stream().mapToInt(Integer::intValue).toArray();
+    public int[] solution(String[] genres, int[] plays) {
+        // 장르별로 재생횟수 집계
+        Map<String, Integer> genre = new HashMap<>();
+        Map<String, List<Integer>> song = new HashMap<>();
+        
+        for (int i = 0; i < genres.length; i++) {
+            genre.put(
+                genres[i],
+                genre.getOrDefault(genres[i], 0) + plays[i]
+            ); // 장르합산
+            
+            song.putIfAbsent(genres[i], new ArrayList<>());
+            song.get(genres[i]).add(i); // 장르에 속한 노래
         }
-        class Music implements Comparable<Music>{
-            int id;
-            int play;
-
-            public Music(int id, int play) {
-                this.id = id;
-                this.play = play;
-            }
-            public int compareTo(Music other){
-                if(this.play == other.play){
-                    return this.id - other.id;
+        
+        // 내림차순한 장르 정렬
+        List<Map.Entry<String, Integer>> genreList = new ArrayList<>(genre.entrySet());
+        genreList.sort((a, b) -> b.getValue() - a.getValue());
+        
+        List<Integer> answerList = new ArrayList<>();
+        
+        // 향상된 for 문 올바른 문법(: 사용) 및 변수명 일치화
+        for (Map.Entry<String, Integer> entry : genreList) {
+            String genKey = entry.getKey();
+            List<Integer> songs = song.get(genKey); // songMap -> song으로 수정
+            
+            songs.sort((a, b) -> {
+                if (plays[a] != plays[b]) {
+                    return plays[b] - plays[a]; // 재생 횟수 내림차순
                 }
-                return other.play - this.play;
+                return a - b; // 고유 번호 오름차순
+            });
+            
+            for (int i = 0; i < Math.min(songs.size(), 2); i++) {
+                answerList.add(songs.get(i)); // answerList로 통일
             }
         }
+        
+        return answerList.stream().mapToInt(i -> i).toArray();
     }
+}
