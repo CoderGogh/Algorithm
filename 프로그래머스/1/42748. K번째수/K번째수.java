9599af6c775b289b3228714f3528eb896d8b4570
Haram Jeong <haram1021@gmail.com>
2026-09-05 15:20:07 +0900

    [level 1] Title: K번째수, Time: 1.70 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,10 +1,16 @@
 import java.util.*;
 class Solution {
-        public int[] solution(int[] array, int[][] commands) {
-            int[] answer = new int[commands.length];
-            for (int i = 0; i < commands.length; i++) {
-                answer[i] = Arrays.stream(Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1])).sorted().toArray()[commands[i][2]-1];
-            }
-            return answer;
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for(int i = 0;i<commands.length; i++){
+            int c[] = commands[i];
+            int first = c[0]-1;
+            int last = c[1];
+            int find = c[2] -1;
+            int[] temp = Arrays.copyOfRange(array,first,last);
+            Arrays.sort(temp);
+            answer[i] = temp[find];
         }
+        return answer;
     }
+}
