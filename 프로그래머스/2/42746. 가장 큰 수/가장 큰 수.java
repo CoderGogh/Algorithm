414c93764c1bebfa522877cbe76c540724b3efcb
Haram Jeong <haram1021@gmail.com>
2026-09-05 15:33:49 +0900

    [level 2] Title: 가장 큰 수, Time: 87.79 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] s = new String[numbers.length];
+        for(int i =0; i<numbers.length; i++){
+            s[i] = String.valueOf(numbers[i]);
+        }
+        Arrays.sort(s,(a,b) -> (b+a).compareTo(a+b));
+        if(s[0].equals("0")){
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for(String st : s){
+            sb.append(st);
+        }
+        return sb.toString();
+    }
+}
