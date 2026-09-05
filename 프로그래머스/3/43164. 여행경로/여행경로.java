1a34f7ef264043bc214705aa550ca33bcbb11768
Haram Jeong <haram1021@gmail.com>
2026-09-05 22:54:06 +0900

    [level 3] Title: 여행경로, Time: 0.70 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,20 +2,18 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 class Solution {
-        boolean visited[];
         String [] answer;
+        boolean[] visited;
         boolean found = false;
-
         public String[] solution(String[][] tickets) {
-            Arrays.sort(tickets,(a,b) -> a[1].compareTo(b[1]));
-
-            visited = new boolean[tickets.length];
+            Arrays.sort(tickets,(a,b)->a[1].compareTo(b[1]));
             List<String> path = new LinkedList<>();
             path.add("ICN");
-            dfs("ICN",path,tickets,0);
+            visited = new boolean[tickets.length];
+            dfs("ICN",tickets,path,0);
             return answer;
         }
-        void dfs(String start, List<String> path, String[][]tickets,int count){
+        void dfs(String start, String[][]tickets, List<String>path,int count){
             if(found){
                 return;
             }
@@ -28,10 +26,9 @@ class Solution {
                 if(!visited[i] && tickets[i][0].equals(start)){
                     visited[i] = true;
                     path.add(tickets[i][1]);
-                    dfs(tickets[i][1],path,tickets,count+1);
-
+                    dfs(tickets[i][1],tickets,path,count+1);
                     path.remove(path.size()-1);
-                    visited[i] =false;
+                    visited[i] = false; // 백트래킹
                 }
             }
         }
