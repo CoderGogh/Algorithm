075398bcd1813572c7f29e8c2539769d9abe3467
Haram Jeong <haram1021@gmail.com>
2026-09-05 21:16:23 +0900

    [level 3] Title: 네트워크, Time: 0.13 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,31 +1,23 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
-class Solution{
-        boolean[] visited;
-        public int solution(int n, int[][]computers) {
-            int answer = 0;
-            visited = new boolean[n];
-            for (int i = 0; i < n; i++) {
-                if(!visited[i]){
-                    answer++;
-                    bfs(i,n,computers);
-                }
+class Solution {
+    int count = 0;
+    boolean[] visited;
+    public int solution(int n, int[][] computers) {
+        visited = new boolean[n];
+        for(int i = 0; i<n;i++){
+            if(!visited[i]){
+                dfs(i,computers, visited);
+                count++;
             }
-            return answer;
         }
-        public void bfs(int startNode,int n, int[][]computers){
-            Deque<Integer> queue = new ArrayDeque<>();
-            visited[startNode] = true;
-            queue.offer(startNode);
-
-            while(!queue.isEmpty()){
-                int node = queue.poll();
-                for (int i = 0; i < n; i++) {
-                    if(computers[node][i] == 1 && !visited[i]){
-                        queue.offer(i);
-                        visited[i] = true;
-                    }
-                }
+        return count;
+    }
+    void dfs(int curr, int[][]computers, boolean[] visited){
+        visited[curr] = true;
+        
+        for(int i = 0;i<computers.length; i++){
+            if(computers[curr][i] == 1 && !visited[i]){
+                dfs(i,computers,visited);
             }
         }
     }
+}
